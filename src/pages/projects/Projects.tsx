88c4dbe19942630f22618a3cc771dcf983fbb10b
Leon Menzies <leon.menzies@hotmail.com>
2023-03-07 20:01:45 +1300

    wip

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,8 +1,20 @@
+import styled from "styled-components";
+
 import ScrumDinger from "pages/projects/projects_data/scrumdinger/ScrumDinger";
+
+const StyledWIP = styled.div`
+  padding: 1rem;
+  text-align: center;
+  color: white;
+  font-size: 1.2rem;
+  color: white;
+`;
 
 const Projects = () => {
   return (
     <div>
+      <StyledWIP>Adding Project is still a work in progress</StyledWIP>
+
       <ScrumDinger />
     </div>
   );
--- src/recoil/theme.ts
+++ src/recoil/theme.ts
@@ -15,7 +15,7 @@ export const darkTheme = {
   mode: "dark",
   primary: "#3c3c3c",
   secondary: "#dc7248",
-  secondaryLight: "#dc724883",
+  secondaryLight: "#dc72485c",
   textPrimary: "#ffffff",
   textSecondary: "#dc7248",
   clear: "rgba(50, 50, 50, 0.85)",
